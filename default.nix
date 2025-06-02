e5da4363c37e9484d61f79c31dccf699ce83827a
SchweGELBin <abramjannikmichael06@gmail.com>
2025-06-03 00:44:36 +0200

    chore: fix packages path

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@
   pkgs ? import <nixpkgs> { },
 }:
 {
-  catspeak = pkgs.callPackage ./catspeak { };
-  smoos-bot = pkgs.callPackage ./smoos/smoos-bot { };
-  smoos-cs = pkgs.callPackage ./smoos/smoos-cs { };
-  smoos-rs = pkgs.callPackage ./smoos/smoos-rs { };
+  catspeak = pkgs.callPackage ./pkgs/catspeak { };
+  smoos-bot = pkgs.callPackage ./pkgs/smoos/smoos-bot { };
+  smoos-cs = pkgs.callPackage ./pkgs/smoos/smoos-cs { };
+  smoos-rs = pkgs.callPackage ./pkgs/smoos/smoos-rs { };
 }
