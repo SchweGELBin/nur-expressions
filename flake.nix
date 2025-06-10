bf09187b843f4a80932af77d50b06e52dbff5e7b
SchweGELBin <abramjannikmichael06@gmail.com>
2025-06-10 17:45:13 +0200

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "nur-expressions";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      systems = lib.systems.flakeExposed;
+      eachSystem = lib.genAttrs systems;
+    in
+    {
+      packages = eachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          nur = pkgs.callPackage ./default.nix { };
+          default = pkgs.callPackage ./default.nix { };
+        }
+      );
+    };
+}
