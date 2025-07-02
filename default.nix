4bbe03a51e2eba4f6bc8fab33f26283313b33502
SchweGELBin <abramjannikmichael06@gmail.com>
2025-07-02 23:19:20 +0200

    pkgs: fix path

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@
   pkgs ? import <nixpkgs> { },
 }:
 {
-  catspeak = pkgs.callPackage ./catspeak { };
-  smoos-bot = pkgs.callPackage ./smoos/bot.nix { };
-  smoos-cs = pkgs.callPackage ./smoos/cs.nix { };
-  smoos-rs = pkgs.callPackage ./smoos/rs.nix { };
+  catspeak = pkgs.callPackage ./pkgs/catspeak { };
+  smoos-bot = pkgs.callPackage ./pkgs/smoos/bot.nix { };
+  smoos-cs = pkgs.callPackage ./pkgs/smoos/cs.nix { };
+  smoos-rs = pkgs.callPackage ./pkgs/smoos/rs.nix { };
 }
