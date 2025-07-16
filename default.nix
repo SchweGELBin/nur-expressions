b7114caca468a3793af38825072d7195e8e0e4b1
SchweGELBin <abramjannikmichael06@gmail.com>
2025-07-16 14:25:55 +0200

    build: use RUSTC_BOOTSTRAP instead of fenix-monthly

--- default.nix
+++ default.nix
@@ -1,18 +1,10 @@
 {
-  fenix ? import (fetchTarball "https://github.com/nix-community/fenix/archive/monthly.tar.gz") { },
   pkgs ? import <nixpkgs> { },
 }:
 
-let
-  rustNightly = pkgs.makeRustPlatform {
-    cargo = fenix.minimal.cargo;
-    rustc = fenix.minimal.rustc;
-  };
-in
-
 {
   catspeak = pkgs.callPackage ./pkgs/catspeak { };
-  mixbot = pkgs.callPackage ./pkgs/mixbot { rustPlatform = rustNightly; };
+  mixbot = pkgs.callPackage ./pkgs/mixbot { };
   smoos-bot = pkgs.callPackage ./pkgs/smoos/bot.nix { };
   smoos-cs = pkgs.callPackage ./pkgs/smoos/cs.nix { };
   smoos-rs = pkgs.callPackage ./pkgs/smoos/rs.nix { };
