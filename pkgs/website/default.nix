e5231a032d903e780e771852d1ce96adaba145cd
SchweGELBin <abramjannikmichael06@gmail.com>
2025-08-05 21:51:03 +0200

    website: 1.1.0 -> 1.1.1

--- pkgs/website/default.nix
+++ pkgs/website/default.nix
@@ -3,8 +3,8 @@ let
   repo = fetchFromGitHub {
     owner = "SchweGELBin";
     repo = "website";
-    tag = "v1.1.0";
-    hash = "sha256-Nr6x+pz+o+KUvmu7IsZse35OyIaAEVRkSFgILLZeK3k=";
+    tag = "v1.1.1";
+    hash = "sha256-KiH0SLRJKE4w++q8Utzo/AmOQ/uUMyPMR7CfVvq+KTE=";
   };
 in
 
