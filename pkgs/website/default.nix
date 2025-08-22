68386d013e14f04dbeef598ea8b0dbebeb75f954
SchweGELBin <abramjannikmichael06@gmail.com>
2025-08-22 02:07:44 +0200

    website: 1.3.0 -> 1.4.0

--- pkgs/website/default.nix
+++ pkgs/website/default.nix
@@ -3,8 +3,8 @@ let
   repo = fetchFromGitHub {
     owner = "SchweGELBin";
     repo = "website";
-    tag = "v1.3.0";
-    hash = "sha256-IQezUomVKot2wV8ws8ZVn9KJE4MkLa1voiQiWmRCJPc=";
+    tag = "v1.4.0";
+    hash = "sha256-iAqcAFVYJFBY1ZEr58DxEQD7+MlvpHYSLUvMiR5/5rI=";
   };
 in
 
