63d36ca1b15e68c9ff639a0cc1c434784900e99e
SchweGELBin <abramjannikmichael06@gmail.com>
2025-11-02 22:46:01 +0100

    style: format modules/nixos/cache

--- modules/nixos/cache.nix
+++ modules/nixos/cache.nix
@@ -5,8 +5,8 @@ in
 {
   config = lib.mkIf cfg.enable {
     nix.settings = {
-      substituters = [ "https://schwegelbin.cachix.org" ];
-      trusted-public-keys = [ "schwegelbin.cachix.org-1:Ckh//WCg8vz3W1AzjD/QdYZ4VHA7ZU/q7nXb98IZ+TQ=" ];
+      substituters = "https://schwegelbin.cachix.org";
+      trusted-public-keys = "schwegelbin.cachix.org-1:Ckh//WCg8vz3W1AzjD/QdYZ4VHA7ZU/q7nXb98IZ+TQ=";
     };
   };
 
