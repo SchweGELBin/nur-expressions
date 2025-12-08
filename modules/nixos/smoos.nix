1f0975e4ce7d01c846bf1ad59d9ac19a54a93122
SchweGELBin <abramjannikmichael06@gmail.com>
2025-12-08 21:13:18 +0100

    modules(smoos): move bot to cs; add rs

--- modules/nixos/smoos.nix
+++ modules/nixos/smoos.nix
@@ -9,22 +9,14 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
-    networking.firewall.allowedTCPPorts = [ ] ++ lib.optionals cfg.cs.enable [ cfg.cs.settings.port ];
+    networking.firewall.allowedTCPPorts =
+      lib.optionals cfg.cs.enable [ cfg.cs.settings.port ]
+      ++ lib.optionals cfg.rs.enable [ cfg.rs.settings.port ];
+    networking.firewall.allowedUDPPorts =
+      lib.optionals cfg.cs.enable [ cfg.cs.settings.port ]
+      ++ lib.optionals cfg.rs.enable [ cfg.rs.settings.port ];
 
     systemd.services = {
-      smoos-bot = {
-        enable = cfg.bot.enable;
-        environment = lib.concatMapAttrs (n: v: {
-          ${"SMOOS_" + lib.toUpper n} = if lib.isBool v then lib.boolToString v else toString v;
-        }) cfg.bot.settings;
-        script = lib.getExe cfg.bot.package;
-        serviceConfig = {
-          EnvironmentFile = cfg.bot.secretFile;
-          User = "smoos";
-          WorkingDirectory = "/var/lib/smoos";
-        };
-        wantedBy = [ "multi-user.target" ];
-      };
       smoos-cs = {
         enable = cfg.cs.enable;
         preStart =
@@ -48,7 +40,70 @@ in
         serviceConfig = {
           EnvironmentFile = cfg.cs.secretFile;
           User = "smoos";
-          WorkingDirectory = "/var/lib/smoos";
+          WorkingDirectory = "/var/lib/smoos/cs";
+        };
+        wantedBy = [ "multi-user.target" ];
+      };
+      smoos-cs-bot = {
+        enable = cfg.cs.bot.enable;
+        environment =
+          lib.concatMapAttrs (n: v: {
+            ${"SMOOS_" + lib.toUpper n} = if lib.isBool v then lib.boolToString v else toString v;
+          }) cfg.cs.bot.settings
+          // {
+            API_HOST = cfg.cs.settings.address;
+            API_PORT = cfg.cs.settings.port;
+          };
+        script = lib.getExe cfg.cs.bot.package;
+        serviceConfig = {
+          EnvironmentFile = cfg.cs.secretFile;
+          User = "smoos";
+          WorkingDirectory = "/var/lib/smoos/cs";
+        };
+        wantedBy = [ "multi-user.target" ];
+      };
+      smoos-rs = {
+        enable = cfg.rs.enable;
+        preStart =
+          lib.optionalString cfg.rs.settings.force ''
+            if [ -f ./settings.json ]; then rm ./settings.json; fi
+          ''
+          + ''
+            if [ ! -f ./settings.json ]; then
+              cp ${cfg.rs.package}/settings.json .
+              chmod +w ./settings.json
+            fi
+            sed -i -e 's/"Address":.*,/"Address": "${cfg.rs.settings.address}",/' settings.json
+            sed -i -e 's/"Port":.*,/"Port": "${toString cfg.rs.settings.port}",/' settings.json
+          ''
+          + lib.optionalString cfg.rs.settings.jsonapi ''
+            sed -i '/JsonApi/{n;s/false/true/}' ./settings.json
+            sed -i -e "s/\"SECRET_TOKEN_1\"/\"$SMOOS_API_TOKEN_PUB\"/g" ./settings.json
+            sed -i -e "s/\"SECRET_TOKEN_2\"/\"$SMOOS_API_TOKEN\"/g" ./settings.json
+          '';
+        script = "${cfg.rs.package}/bin/smo-rs";
+        serviceConfig = {
+          EnvironmentFile = cfg.rs.secretFile;
+          User = "smoos";
+          WorkingDirectory = "/var/lib/smoos/rs";
+        };
+        wantedBy = [ "multi-user.target" ];
+      };
+      smoos-rs-bot = {
+        enable = cfg.rs.bot.enable;
+        environment =
+          lib.concatMapAttrs (n: v: {
+            ${"SMOOS_" + lib.toUpper n} = if lib.isBool v then lib.boolToString v else toString v;
+          }) cfg.rs.bot.settings
+          // {
+            API_HOST = cfg.rs.settings.address;
+            API_PORT = cfg.rs.settings.port;
+          };
+        script = lib.getExe cfg.rs.bot.package;
+        serviceConfig = {
+          EnvironmentFile = cfg.rs.secretFile;
+          User = "smoos";
+          WorkingDirectory = "/var/lib/smoos/rs";
         };
         wantedBy = [ "multi-user.target" ];
       };
@@ -65,51 +120,31 @@ in
   options = {
     nur.smoos = {
       enable = lib.mkEnableOption "Enable Super Mario Odyssey: Online Server";
-      bot = {
-        enable = lib.mkEnableOption "Enable Super Mario Odyssey: Online Server - Bot";
-        package = lib.mkPackageOption pkgs "smoos-bot" { };
-        secretFile = lib.mkOption {
-          description = ''
-            File containing environment variables
-            Needs `SMOOS_API_TOKEN`, `SMOOS_DISCORD_TOKEN`
-          '';
-          example = "config.sops.secrets.smoos_env.path";
-          type = lib.types.path;
-        };
-        settings = lib.mkOption {
-          description = "Configuration options for the Server Bot";
-          type = lib.types.submodule {
-            options = {
-              api_host = lib.mkOption {
-                default = "localhost";
-                description = "Your Server IP";
-                example = "example.com";
-                type = lib.types.str;
-              };
-              api_port = lib.mkOption {
-                default = 1027;
-                description = "Your Server Port";
-                example = 1028;
-                type = lib.types.int;
-              };
-              discord_id = lib.mkOption {
-                description = "Your Discord User ID";
-                example = "123456789012345678";
-                type = lib.types.str;
-              };
-              discord_prefix = lib.mkOption {
-                default = "!";
-                description = "Your Discord Bot Command Prefix";
-                example = ".";
-                type = lib.types.str;
+      cs = {
+        enable = lib.mkEnableOption "Enable Super Mario Odyssey: Online Server - C#";
+        package = lib.mkPackageOption pkgs "smoos-cs" { };
+        bot = {
+          enable = lib.mkEnableOption "Enable Super Mario Odyssey: Online Server - Bot";
+          package = lib.mkPackageOption pkgs "smoos-bot" { };
+          settings = lib.mkOption {
+            description = "Configuration options for the Server Bot";
+            type = lib.types.submodule {
+              options = {
+                discord_id = lib.mkOption {
+                  description = "Your Discord User ID";
+                  example = "123456789012345678";
+                  type = lib.types.str;
+                };
+                discord_prefix = lib.mkOption {
+                  default = "!";
+                  description = "Your Discord Bot Command Prefix";
+                  example = ".";
+                  type = lib.types.str;
+                };
               };
             };
           };
         };
-      };
-      cs = {
-        enable = lib.mkEnableOption "Enable Super Mario Odyssey: Online Server - C#";
-        package = lib.mkPackageOption pkgs "smoos-cs" { };
         secretFile = lib.mkOption {
           description = ''
             File containing environment variables
@@ -138,8 +173,61 @@ in
             };
           };
         };
+        rs = {
+          enable = lib.mkEnableOption "Enable Super Mario Odyssey: Online Server - Rust";
+          package = lib.mkPackageOption pkgs "smoos-rs" { };
+          bot = {
+            enable = lib.mkEnableOption "Enable Super Mario Odyssey: Online Server - Bot";
+            package = lib.mkPackageOption pkgs "smoos-bot" { };
+            settings = lib.mkOption {
+              description = "Configuration options for the Server Bot";
+              type = lib.types.submodule {
+                options = {
+                  discord_id = lib.mkOption {
+                    description = "Your Discord User ID";
+                    example = "123456789012345678";
+                    type = lib.types.str;
+                  };
+                  discord_prefix = lib.mkOption {
+                    default = "!";
+                    description = "Your Discord Bot Command Prefix";
+                    example = ".";
+                    type = lib.types.str;
+                  };
+                };
+              };
+            };
+          };
+          secretFile = lib.mkOption {
+            description = ''
+              File containing environment variables
+              Needs `SMOOS_API_TOKEN`, `SMOOS_API_TOKEN_PUB` if `nur.smoos.cs.settings.jsonapi = true`
+            '';
+            example = "config.sops.secrets.smoos_env.path";
+            type = lib.types.path;
+          };
+          settings = lib.mkOption {
+            description = "Configuration options for the Rust Server";
+            type = lib.types.submodule {
+              options = {
+                address = lib.mkOption {
+                  default = "0.0.0.0";
+                  description = "Your Server Adress";
+                  type = lib.types.str;
+                };
+                force = lib.mkEnableOption "Replace existing settings";
+                jsonapi = lib.mkEnableOption "Enable the JsonApi, necessary if you want to use smoos-bot";
+                port = lib.mkOption {
+                  default = 1027;
+                  description = "Your Server Port";
+                  example = 1028;
+                  type = lib.types.int;
+                };
+              };
+            };
+          };
+        };
       };
-      rs.enable = lib.mkEnableOption "Enable Super Mario Odyssey: Online Server - Rust";
     };
   };
 }
