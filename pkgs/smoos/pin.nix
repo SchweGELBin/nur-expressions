4f9af0b5b39d5343c8804d9dd3f36cd809d1f1c3
SchweGELBin <abramjannikmichael06@gmail.com>
2025-12-12 17:18:47 +0100

    smoos: 1.3.3 -> 1.3.4

--- pkgs/smoos/pin.nix
+++ pkgs/smoos/pin.nix
@@ -1,5 +1,5 @@
 {
-  version = "v1.3.3";
-  hash = "sha256-0Nz5z1ThTPG0vxPXpx3wv7TL9EsNDbaFDa97Rseq9O4=";
-  subHash = "sha256-yFEwPfekKncXTF3+d2QE/3W/W2z7PgTmxCIuuRySfgo=";
+  version = "v1.3.4";
+  hash = "sha256-jcae3aKxYOIIoINVAxlvMvW5dP033/PEUmrjeVz3G8k=";
+  subHash = "sha256-RZ/9nRjNAO40J4kbY/NSjyaMfOexBEs5BjeAibvZZVQ=";
 }
