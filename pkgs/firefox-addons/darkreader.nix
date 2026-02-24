315ab0872a9898292bc4e229f278c2cb60a4e458
SchweGELBin <abramjannikmichael06@gmail.com>
2026-02-24 20:02:13 +0100

    firefox-addons.darkreader: use xpi from Mozilla's store
    The GitHub builds are broken

--- pkgs/firefox-addons/darkreader.nix
+++ pkgs/firefox-addons/darkreader.nix
@@ -8,9 +8,17 @@ buildXpi rec {
   pname = "darkreader";
   version = "4.9.121";
 
+  /*
+    # The GitHub Releases are broken
+    src = fetchurl {
+      url = "https://github.com/darkreader/darkreader/releases/download/v${version}/darkreader-firefox.xpi";
+      hash = "sha256-yD0zFlyVDOMzqTfyIzjqIhRhiI+4YMfjYi3SOmV4hDE=";
+    };
+  */
+
   src = fetchurl {
-    url = "https://github.com/darkreader/darkreader/releases/download/v${version}/darkreader-firefox.xpi";
-    hash = "sha256-yD0zFlyVDOMzqTfyIzjqIhRhiI+4YMfjYi3SOmV4hDE=";
+    url = "https://addons.mozilla.org/firefox/downloads/file/4690921/darkreader-${version}.xpi";
+    hash = "sha256-wOxdUE6XHfvq3cRvh5x0Cl9MfAN6Z47NMZr7eYb+Y+s=";
   };
 
   addonId = "addon@darkreader.org";
