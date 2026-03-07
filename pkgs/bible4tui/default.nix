8ae7980d2905eb9260970f99481227a74acd0d5c
SchweGELBin <abramjannikmichael06@gmail.com>
2026-03-07 17:46:45 +0100

    bible4tui: 0.4.0 -> 0.5.0

--- pkgs/bible4tui/default.nix
+++ pkgs/bible4tui/default.nix
@@ -3,8 +3,8 @@ let
   src = fetchFromGitHub {
     owner = "SchweGELBin";
     repo = "Bible4TUI";
-    tag = "v0.4.0";
-    hash = "sha256-KGkp66AkEFL+c1LN48OkMVPAYHYbPH0jrmvJoqSH6z0=";
+    tag = "v0.5.0";
+    hash = "sha256-qfrPwzr9Q4fuhlJSWAmL9F97A4uXUd5QZhvPvBriiFI=";
   };
 in
 
