fde260278a864e76b08ddb0cdc02cd99966b0785
SchweGELBin <abramjannikmichael06@gmail.com>
2026-03-09 21:10:21 +0100

    firefox-addons.darkreader: 4.9.121 -> 4.9.123

--- pkgs/firefox-addons/darkreader.nix
+++ pkgs/firefox-addons/darkreader.nix
@@ -6,21 +6,20 @@
 
 buildXpi rec {
   pname = "darkreader";
-  version = "4.9.121";
+  version = "4.9.123";
 
   /*
     # The GitHub Releases are broken
     src = fetchurl {
       url = "https://github.com/darkreader/darkreader/releases/download/v${version}/darkreader-firefox.xpi";
-      hash = "sha256-yD0zFlyVDOMzqTfyIzjqIhRhiI+4YMfjYi3SOmV4hDE=";
+      hash = "sha256-UcFs3oXn0cLW0WLD9a4mI3nNaH2sg+PtX+tVCjL/8U8=";
     };
   */
 
   src = fetchurl {
-    url = "https://addons.mozilla.org/firefox/downloads/file/4690921/darkreader-${version}.xpi";
-    hash = "sha256-wOxdUE6XHfvq3cRvh5x0Cl9MfAN6Z47NMZr7eYb+Y+s=";
+    url = "https://addons.mozilla.org/firefox/downloads/file/4710145/darkreader-${version}.xpi";
+    hash = "sha256-mj5s2sU/ICV72BJkUywWr+2tAOsmI73tie0rMLHkAkM=";
   };
-
   addonId = "addon@darkreader.org";
 
   meta = {
