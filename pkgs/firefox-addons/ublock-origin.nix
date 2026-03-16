2580a0f2a30d4bbbb4cc8d273f95ddd6a2d6c37d
SchweGELBin <abramjannikmichael06@gmail.com>
2026-03-16 02:37:09 +0100

    firefox-addons.ublock-origin: 1.69.0 -> 1.70.0

--- pkgs/firefox-addons/ublock-origin.nix
+++ pkgs/firefox-addons/ublock-origin.nix
@@ -6,11 +6,11 @@
 
 buildXpi rec {
   pname = "ublock-origin";
-  version = "1.69.0";
+  version = "1.70.0";
 
   src = fetchurl {
     url = "https://github.com/gorhill/uBlock/releases/download/${version}/uBlock0_${version}.firefox.signed.xpi";
-    hash = "sha256-eFvN5ool+qiglJlk7F/+m9y4XT8K4hwj9gfGyPkUcs8=";
+    hash = "sha256-8nMNKHcAV2OkXXZXSYkuk29JyucT0o96puoxRFS4nPE=";
   };
 
   addonId = "uBlock0@raymondhill.net";
