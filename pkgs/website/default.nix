1d731756513dfb2f4fb36ca0e2ae7e5b6bd64102
SchweGELBin <schwegelbin@gmail.com>
2026-05-18 13:07:04 +0200

    website: 1.6.11 -> 1.6.12

--- pkgs/website/default.nix
+++ pkgs/website/default.nix
@@ -3,8 +3,8 @@ let
   src = fetchFromGitHub {
     owner = "SchweGELBin";
     repo = "website";
-    tag = "v1.6.11";
-    hash = "sha256-VGjGrIfAn40jfUxjUqAEJvwsVad8Cf2okXPAkQroF2g=";
+    tag = "v1.6.12";
+    hash = "sha256-+R6f5i8EsuKbqj7Eu0JGXmsf43HSM68PyhIThoembYQ=";
   };
 in
 
