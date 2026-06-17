ea3798a900ec992182915a43bbe18bf45f9de718
SchweGELBin <schwegelbin@gmail.com>
2026-06-17 19:32:00 +0200

    smoos: 1.4.1 -> 1.4.2

--- pkgs/smoos/pin.nix
+++ pkgs/smoos/pin.nix
@@ -1,5 +1,5 @@
 {
-  version = "v1.4.1";
-  hash = "sha256-esQ/0gyGc8Z8+2SWURElPt9y6N8oSIoH8tE55ig7e3Q=";
-  subHash = "sha256-O0MQX7Ir8Urik1FYSlcicRnFijCvwt7qKrrfvyALt80=";
+  version = "v1.4.2";
+  hash = "sha256-RP0r4HkIWRIIBhjBCzN7btXlsK41hoOJeWEWXXgWNuI=";
+  subHash = "sha256-PtAt08pIwiYhNpzFkXF+BDIm3zojRbVFmwWPxK05q3E=";
 }
